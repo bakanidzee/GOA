cceac0d8d164a00ce19ee78206062893bc10f39d
bakanidze <bakanidzetemuka@gmail.com>
2024-10-20 15:46:43 +0400

    second commit

---
+++ day 004/classwork/cw.py
@@ -0,0 +1,34 @@
+#1) შექმენით ყველა მონაცემთა ტიპის ცვლადი შემდეგ კი დაბეჭდეთ,შემდეგ შექმენით 2 ცვლადი რომლის მნიჭვნელობაც იქნება ინტეჯერები შემდეგ კი ამ რიცხვებზე შეასრულეთ ყველა მათემათიკური მოქმედება
+
+name = "temo"
+surname = "bakanidze"
+age = "14"
+height = "160"
+
+num1 = 60
+num2 = 5
+num3 = 5.5
+num4 = 3.3
+
+print(name)
+
+print(surname)
+
+print(age)
+
+print(height)
+
+
+print("i am " + name + " " +surname + ". i am " + str(age) + " years " + "and " + str(height)+ " height.")
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+print(num3 + num4)
+print(num3 - num4)
+print(num3 * num4)
+print(num3 / num4)
---
+++ day 005/homework/hw2.py
@@ -0,0 +1,9 @@
+#2) დაადეკლარირეთ სტრინგ ტიპის ცვლადი სახელად name რომელსაც მიანიჭებთ მნიშვნელობად თქვენს სახელს
+
+
+name = "temo"
+
+surname = "bakanidze"
+
+print(name + surname)
+print("temo" + "bakanidze")
